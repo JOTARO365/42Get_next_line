c987314bc138aa5bb22aac6fd979ae8f44c8e10c
Wisanu Aon-In <108912837+JOTARO365@users.noreply.github.com>
2023-12-06 03:06:17 +0700

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -3,17 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: waon-in <waon-in@student.42>               +#+  +:+       +#+        */
+/*   By: waon-in <waon-in@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 23:58:36 by waon-in           #+#    #+#             */
-/*   Updated: 2023/12/05 00:09:14 by waon-in          ###   ########.fr       */
+/*   Updated: 2023/12/06 02:49:54 by waon-in          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
 
+size_t		ft_strlen(const char *s);
+char		*ft_strjoin(const char *s1 , const char *2);
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,12 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: waon-in <waon-in@student.42>               +#+  +:+       +#+        */
+/*   By: waon-in <waon-in@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 23:58:40 by waon-in           #+#    #+#             */
-/*   Updated: 2023/12/05 00:00:05 by waon-in          ###   ########.fr       */
+/*   Updated: 2023/12/06 03:05:41 by waon-in          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int				i;
+	int				j;
+	int				size;
+	char		*str;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc((size + 1) * sizeof(char));
+	if (!s1 || !s2 || !str)
+		return (NULL);
+	i = 0;
+	while (s1[i] != 0)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != 0)
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str(size) = 0;
+	return (str);
+}
